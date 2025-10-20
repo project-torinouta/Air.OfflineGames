d60ea5a3bf51ec1a7d963ad343b160bf545feea9
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-10-21 02:17:28 +0800

    :loud_sound: debug(log): adding debug log files and fix the test collision before clearing problem

--- tetro/tetro.sh
+++ tetro/tetro.sh
@@ -5,6 +5,15 @@ stty -echo -icanon
 true=1
 false=0
 
+log_filename="$( pwd )/$( date +"%Y-%m-%d_%H-%M-%S" ).log"
+show_log=$false
+
+if [[ $1 == "--show-log" ]]; then
+  show_log=$true
+  rm ./*.log
+  touch $log_filename
+fi
+
 # Notice the color defined below is just for my kitty + everforest terminal, if
 # you don't adjust to them you can also modify.
 
@@ -100,12 +109,21 @@ next_tetromino_name=""
 # This procedure initializes the playfield and use one-dimensional array to
 # simulate the two-dimensional array.
 
-for ((i=0; i<$(( $playfield_height + $tetromino_block_length )); i++)); do
-  for ((j=0; j<$playfield_width; j++)); do
+for ((i=0; i < $(( $playfield_height + $tetromino_block_length )); i++)); do
+  for ((j=0; j < $playfield_width; j++)); do
     index=$(( $i * $playfield_height + $j))
     playfield[$index]=0
   done
 done
+
+function log_file() {
+  local level=$1
+  local message=$2
+
+  if [[ $show_log -eq $true ]]; then
+    printf "[%s] [%-5s] : %s\n" "$( date +"%Y-%m-%d %H:%M:%S:%N")" "$level" "$message" >> $log_filename
+  fi
+}
 
 # Bash cannot pass array into functions but can use references.
 # Bash cannot return array from functions but can use references.
@@ -134,6 +152,8 @@ function new_tetromino() {
   current_y=0
   current_x=$(( ($playfield_width - $tetromino_block_length) / 2 ))
   current_color=$( get_random_element color_foreground )
+
+  log_file "debug" "Reset the new tetromino to ($current_x, $current_y)"
 }
 new_tetromino
 
@@ -169,6 +189,29 @@ function test_collision() {
     for ((j=0; j < $tetromino_block_length; j++)); do
       local test_x=$(( $x + $j ))
       local test_y=$(( $y + $i ))
+
+      log_file \
+        "debug" \
+        "Beginning to debug the collision test procedure for position ($j, $i) ..."
+
+      if [[ ${current_tetromino[$(( $i * $tetromino_block_length + $j ))]} -eq 1 ]]; then
+        log_file \
+          "debug" \
+          "Debugging position ($j, $i) in current tetromino and its a block"
+      fi
+
+      if [[ ${playfield[$(( $test_y * $playfield_width + $test_x ))]} -ne 0 ]]; then
+        log_file \
+          "debug" \
+          "Debugging position ($j, $i) in current tetromino, current test
+x of tetromino is $x, y of tetromino is $y and position \
+($test_x, $test_y) of playfield is already filled."
+      fi
+
+      log_file \
+        "debug" \
+        "Finishing to debug the collision test procedure for position ($j, $i) ..."
+
       if [[ \
         ${current_tetromino[$(( $i * $tetromino_block_length + $j ))]} -eq 1 && \
         (${playfield[$(( $test_y * $playfield_width + $test_x ))]} -ne 0 || \
@@ -176,11 +219,15 @@ function test_collision() {
          $test_x -lt 0 || \
          $test_x -ge $playfield_width)
       ]]; then
+        log_file "debug" "Function 'test_collision' returned true"
         printf "%d" $true
+        return 0
       fi
     done
   done
+  log_file "debug" "Function 'test_collision' returned false"
   printf "%d" $false
+  return 0
 }
 
 function update_game() {
@@ -258,17 +305,7 @@ function arrow_control() {
   direction=$1
   condition=$false
 
-  case "$direction" in
-    $"left")
-      condition=$( test_collision $(( $current_x - 1 )) $current_y )
-      ;;
-    $"right")
-      condition=$( test_collision $(( $current_x + 1 )) $current_y )
-      ;;
-    $"down")
-      condition=$( test_collision $current_x $(( $current_y + 1)) )
-      ;;
-  esac
+  log_file "debug" "Read the arrow key '$direction'"
 
   for ((i=0; i < $tetromino_block_length; i++)); do
     for ((j=0; j < $tetromino_block_length; j++)); do
@@ -280,18 +317,40 @@ function arrow_control() {
     done
   done
 
+  # Dev-Log: 2025-10-21 02-09-13 The last block must be first cleared before
+  # testing the collision
+
+  case "$direction" in
+    left)
+      condition=$( test_collision $(( $current_x - 1 )) $current_y )
+      log_file "debug" "Match the case LeftArrow"
+      ;;
+    right)
+      condition=$( test_collision $(( $current_x + 1 )) $current_y )
+      log_file "debug" "Match the case RightArrow"
+      ;;
+    down)
+      condition=$( test_collision $current_x $(( $current_y + 1)) )
+      log_file "debug" "Math the case DownArrow"
+      ;;
+  esac
+
+  log_file "debug" "The 'condition' variable has been set to $condition"
+
   if [[ $condition -eq $false ]]; then
     case "$direction" in
-      $"left")
+      left)
         current_x=$(( $current_x - 1))
         ;;
-      $"right")
+      right)
         current_x=$(( $current_x + 1))
         ;;
-      $"down")
+      down)
         current_y=$(( $current_y + 1))
         ;;
     esac
+
+    log_file "debug" "Move the current tetromino to ($current_x, $current_y)"
 
     for ((i=0; i < $tetromino_block_length; i++)); do
       for ((j=0; j < $tetromino_block_length; j++)); do
@@ -317,14 +376,16 @@ function arrow_control() {
 
 echo -e "\033c"
 tput civis
-trap "stty sane; tput cnorm; echo -e '\nExit Tetro by user input'; exit 0" SIGINT SIGTERM EXIT
+trap "stty echo icanon; tput cnorm; echo -e '\nExit Tetro by user input'; exit 0" SIGINT SIGTERM EXIT
 
 last_update=$( date +%s%N )
 while [ $is_game_over -ne $true ]; do
-  if read -n 1 -t 0.005 -r key; then
+  if read -n 1 -t 0.05 -r key; then
+    log_file "debug" "Read the key '$key' from keyboard"
+
     case "$key" in
-      $'\e')
-        if read -n 2 -t 0.005 -r esc_sequence; then
+      $'\x1B')
+        if read -n 2 -t 0.05 -r esc_sequence; then
           case "$esc_sequence" in
             "[A") ;;
             "[B") arrow_control "down";  draw_game ;;
